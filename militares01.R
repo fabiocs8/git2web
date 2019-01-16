6eaf029d01ac583a37359d4e39372615f5c01137
Fabio Correa <fabiocs.correa@gmail.com>
2019-01-16 17:49:48 -0200

    reducing data

--- militares01.R
+++ militares01.R
@@ -3,22 +3,22 @@ library(tidyverse)
 
 
 ObservacoesFiles = list.files(pattern="*bservacoesMilitares.csv")
-CadastroFiles <- list.files(pattern="*adastroMilitares.csv")
-RemuneracaoFiles <- list.files(pattern="*racaoMilitares.csv")
+# CadastroFiles <- list.files(pattern="*adastroMilitares.csv")
+# RemuneracaoFiles <- list.files(pattern="*racaoMilitares.csv")
 HonorariosFiles <- list.files(pattern="*)Militares.csv")
 
 
 
 
 OBS = data.table(lapply(ObservacoesFiles, fread,dec = ",",check.names = T))
-CAD = data.table(lapply(CadastroFiles, fread,dec = ",",check.names = T))
-REM = data.table(lapply(RemuneracaoFiles, fread,dec = ",",check.names = T))
+# CAD = data.table(lapply(CadastroFiles, fread,dec = ",",check.names = T))
+# REM = data.table(lapply(RemuneracaoFiles, fread,dec = ",",check.names = T))
 HON = data.table(lapply(HonorariosFiles, fread,dec = ",",check.names = T))
 
 #unnestHon <- unnest(HON)
 HON <- rbindlist(HON$V1)
-CAD <- rbindlist(CAD$V1)
-REM <- rbindlist(REM$V1)
+# CAD <- rbindlist(CAD$V1)
+# REM <- rbindlist(REM$V1)
 OBS <- rbindlist(OBS$V1)
 
 REM[-.N]
