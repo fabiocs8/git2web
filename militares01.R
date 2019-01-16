69f0abce01bd6075ec2547cdb1db1217dea8f267
Fabio Correa <fabiocs.correa@gmail.com>
2019-01-16 17:44:38 -0200

    adding files

---
+++ militares01.R
@@ -0,0 +1,31 @@
+library(data.table)
+library(tidyverse)
+
+
+ObservacoesFiles = list.files(pattern="*bservacoesMilitares.csv")
+CadastroFiles <- list.files(pattern="*adastroMilitares.csv")
+RemuneracaoFiles <- list.files(pattern="*racaoMilitares.csv")
+HonorariosFiles <- list.files(pattern="*)Militares.csv")
+
+
+
+
+OBS = data.table(lapply(ObservacoesFiles, fread,dec = ",",check.names = T))
+CAD = data.table(lapply(CadastroFiles, fread,dec = ",",check.names = T))
+REM = data.table(lapply(RemuneracaoFiles, fread,dec = ",",check.names = T))
+HON = data.table(lapply(HonorariosFiles, fread,dec = ",",check.names = T))
+
+#unnestHon <- unnest(HON)
+HON <- rbindlist(HON$V1)
+CAD <- rbindlist(CAD$V1)
+REM <- rbindlist(REM$V1)
+OBS <- rbindlist(OBS$V1)
+
+REM[-.N]
+
+dt <- REM[CAD,on = "Id_SERVIDOR_PORTAL"]
+
+dt[MES==3, .(mean(REMUNERAÇÃO.BÁSICA.BRUTA..R..,na.rm = T),.N), by = "DESCRICAO_CARGO"][order(-V1)]
+
+dt<- dt[OBS,on = "Id_SERVIDOR_PORTAL"]
+dt<- dt[HON,on = "Id_SERVIDOR_PORTAL"]
